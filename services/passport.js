87d575874f4bab3d43dfd37ed6c50c9b2a3b19c3
Pinkel Gurung <pinkelgurung@hotmail.com>
2017-12-03 13:34:55 -0500

    use proxy

--- services/passport.js
+++ services/passport.js
@@ -5,41 +5,36 @@ const keys = require('../config/keys');
 
 const user = mongoose.model('users');
 
-
 passport.serializeUser((user, done) => {
-    done(null, user.id);
+	done(null, user.id);
 });
 
 passport.deserializeUser((id, done) => {
-
-    user.findById(id)
-        .then(user => {
-            done(null, user);
-        })
-
+	user.findById(id).then((user) => {
+		done(null, user);
+	});
 });
 passport.use(
-    new GoogleStrategy(
-        {
-            clientID: keys.google_client_id,
-            clientSecret: keys.google_client_secret,
-            callbackURL: '/auth/google/callback'
-        }, (accessToken, refreshToken, profile, done) => {
-            user.findOne({ 'google_id': profile.id })
-                .then((existingUser) => {
-                    if (existingUser) {
-                        done(null, existingUser);
-                    } else {
-                        new user({ google_id: profile.id }).save()
-                            .then(user => {
-                                done(null, user);
-                            });
-                    }
-                });
-            // console.log("access Token : " + accessToken);
-            // console.log("refreshToken Token : " + refreshToken);
-            // console.log("profile : " + JSON.stringify(profile));
-        }
-    )
+	new GoogleStrategy(
+		{
+			clientID: keys.google_client_id,
+			clientSecret: keys.google_client_secret,
+			callbackURL: '/auth/google/callback',
+			proxy: true
+		},
+		(accessToken, refreshToken, profile, done) => {
+			user.findOne({ google_id: profile.id }).then((existingUser) => {
+				if (existingUser) {
+					done(null, existingUser);
+				} else {
+					new user({ google_id: profile.id }).save().then((user) => {
+						done(null, user);
+					});
+				}
+			});
+			// console.log("access Token : " + accessToken);
+			// console.log("refreshToken Token : " + refreshToken);
+			// console.log("profile : " + JSON.stringify(profile));
+		}
+	)
 );
-
